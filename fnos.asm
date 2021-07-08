519b694af4ac101c56557ae714b8b30ca136717f
Febriyanto Nugroho <febnug@gnuweeb.org>
2021-07-08 10:17:20 +0700

    Update fnos.asm
    sementara dulu kaya gini

--- fnos.asm
+++ fnos.asm
@@ -7,74 +7,70 @@
 
 %include "lib/base.inc"
 %include "game/tetranglix.asm"
+%include "lib/main.asm"
+
 ORG BASE_RUN_OFS
 
 BITS 16
 
 start:
-    call    bersihkan_layar
-    
-    mov     ah, 0x6    
+
+    call    bersihkan_layar_0
+
+    mov     ah, 0x6   
     xor     al, al    
     xor     cx, cx     
     mov     dx, 0x184F  
     mov     bh, 0x0F    
     int     0x10
 
-    mov     ah, 0eh
-    mov     al, ">"
-    int     10h
 
-    mov     ah, 0eh
-    mov     al, " "
-    int     10h
+; DB
+; DC
+; DE
+; DD
+; DF
 
- _loop:
+    
+
+    mov     ah, 0x02
+    mov     dh, 10
+    mov     dl, 38
+    xor     bh, bh
+    int     0x10  
+      
+    mov     ah, 0x09
+    mov     al, 0xFE
+    xor     bh, bh
+    mov     bl, 0x0F
+    mov     cx, 1
+    int     0x10
+
+    mov     ah, 0x02
+    mov     dh, 09
+    mov     dl, 39
+    xor     bh, bh
+    int     0x10  
+      
+    mov     ah, 0x09
+    mov     al, 0xFE
+    xor     bh, bh
+    mov     bl, 0x0F
+    mov     cx, 1
+    int     0x10
+    
+ 
     xor     ah, ah
-    int     16h 
-    mov     ah, 0eh
-    int     10h
-    mov     bl, al 
-    jmp     tukar_char
-lanjut:
-    cmp     al, 0dh
-    je      compare_input
-    jmp     _loop
-tukar_char:
-    xchg    cl, bl
-    jmp     lanjut   
-compare_input:
-    cmp     bl, "t"
-    je      tetris
-    cmp     bl, "T"
-    je      tetris
-    cmp     bl, "s"
-    je      shutdown
-    cmp     bl, "S"
-    je      shutdown 
-    cmp     bl, "r"
-    je      reboot
-    cmp     bl, "R"
-    je      reboot
+    int     0x16 
+    cmp     al, 0x0d
+    je      main_os
+    
 
-    jmp     $            ; Jump here - infinite loop!
+    jmp     $
 
 
-bersihkan_layar:
+bersihkan_layar_0:
     xor     ah, ah
-    mov     al, 0x03  ; text mode 80x25 16 colours
+    mov     al, 0x03  
     int     0x10
     ret
-
-reboot:
-    xor     ah, ah
-    int     19h
-
-shutdown:
-    mov     ax, 0x1000
-    mov     ax, ss
-    mov     sp, 0xf000
-    mov     ax, 0x5307
-    mov     bx, 0x0001
-    mov     cx, 0x0003
-    int     0x15
