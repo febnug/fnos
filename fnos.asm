fcfcd4ae66e3ffab5f4e19cc789de83bd59de3c9
Febriyanto Nugroho <vmvm1071@gmail.com>
2021-08-12 14:30:47 +0700

    proyek gak jelas :)

--- fnos.asm
+++ fnos.asm
@@ -2,12 +2,13 @@
 ; |   __|   | |     |   __|
 ; |   __| | | |  |  |__   |
 ; |__|  |_|___|_____|_____|
-;               v.0.0-beta
+;
+; Copyright (c) 2021 Febriyanto Nugroho
 
 
 %include "lib/base.inc"
 %include "game/tetranglix.asm"
-%include "lib/main.asm"
+%include "lib/fungsi.asm"
 
 ORG BASE_RUN_OFS
 
@@ -15,31 +16,73 @@ BITS 16
 
 start:
 
-    call    bersihkan_layar_0
+main_os:
+    call    bersihkan_layar_1
 
-    mov     ah, 0x6   
-    xor     al, al    
-    xor     cx, cx     
-    mov     dx, 0x184F  
-    mov     bh, 0x0F    
+    mov     ah, 0x6    ; Scroll up function
+    xor     al, al    ; Clear entire screen
+    xor     cx, cx     ; Upper left corner CH=row, CL=column
+    mov     dx, 0x184F  ; lower right corner DH=row, DL=column
+    mov     bh, 0x0F    ; Black & White
     int     0x10
 
-; TODO :
-;   1. need something useless
 
-    jmp     main_os   
+
+    mov     ah, 0x02
+    mov     dh, 0x09
+    xor     dl, dl
+    xor     bh, bh
+    int     0x10
  
-;    xor     ah, ah
-;    int     0x16 
-;    cmp     al, 0x0d
-;    je      main_os
-    
 
- ;   jmp     $
+    mov     si, intro
+    call    cetak_string   
 
 
-bersihkan_layar_0:
-    xor     ah, ah
-    mov     al, 0x03  
+    mov     ah, 0x02
+    mov     dh, 0x18
+    xor     dl, dl
+    xor     bh, bh
     int     0x10
-    ret
+
+    mov     ah, 0x01
+    mov     cx, 16
+    int     0x10
+
+_loop:
+    xor     ah, ah
+    int     0x16 
+_cetak:
+    mov     ah, 0x0e
+    int     0x10
+    cmp     al, 0x08
+    je      _backspace
+    test    al, al
+    je      _input_lagi
+    mov     bl, al 
+    jmp     tukar_char
+lanjut:
+    cmp     al, 0x0d
+    je      compare_input
+    jmp     _loop
+tukar_char:
+    xchg    cl, bl
+    jmp     lanjut   
+compare_input:
+    cmp     bl, "t"
+    je      tetris
+    cmp     bl, "T"
+    je      tetris
+_backspace: 
+    xor     al, al
+    jmp     _cetak 
+_input_lagi:
+    mov     ah, 0x02
+    mov     dh, 0x18
+    mov     dl, 0x00
+    xor     bh, bh
+    int     0x10 
+    jmp     _loop
+
+
+    jmp     $
