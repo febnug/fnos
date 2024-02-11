b2cc6d5d206a522acd80f8068d0d3fe5bbc8c1e7
Febriyanto Nugroho <vmvm1071@gmail.com>
2024-02-11 11:35:06 +0700

    Update os.asm
    finally ! protected mode ?

--- os.asm
+++ os.asm
@@ -18,6 +18,72 @@ ORG 0x7c00
 ; Include a BPB (1.44MB floppy with FAT12) to be more comaptible with USB floppy media
 %include "lib/bpb.inc"
 
+; yo!, protected mode guys :)
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+__start:
+	jmp	short _start
+	nop
+	times 33 db 0
+_start:
+	jmp	0x0:start
+start:
+	cli
+	xor	ax, ax
+	mov	ds, ax
+	mov	es, ax
+
+	xor	ax, ax
+	mov	ss, ax
+	mov	sp, 0x7c00
+	sti
+
+.load_protected:
+	cli
+	lgdt	[gdt_descriptor]
+	; Whee, we are entering the protected mode!
+	mov	eax, cr0
+	or	eax, 1
+	mov	cr0, eax
+	jmp	CODE_SEG:boot_start
+
+
+; GDT
+gdt_start:
+
+gdt_null:
+	dd	0x0
+	dd	0x0
+	; Offset 0x8
+gdt_code:			; CS should point to this label.
+	dw	0xffff		; Segment limit first 0-15 bit.
+	dw	0x0		; Base first 0-15 bits.
+	db	0x0		; Base 16-23 bits.
+	db	0x9a		; Access byte.
+	db	11001111b	; High 4 bit flags and low 4 bit flags.
+	db	0x0		; Base 24-31 bits.
+	; Offset 0x10
+
+gdt_data:			; DS, SS, ES, FS, GS
+	dw	0xffff		; Segment limit first 0-15 bit.
+	dw	0x0		; Base first 0-15 bits.
+	db	0x0		; Base 16-23 bits.
+	db	0x92		; Access byte.
+	db	11001111b	; High 4 bit flags and low 4 bit flags.
+	db	0x0		; Base 24-31 bits.
+gdt_end:
+
+gdt_descriptor:
+	dw	gdt_end - gdt_start - 1
+	dd	gdt_start
+
+
+; ------------------------------------------------------------------------------
+
+
+
 boot_start:
     xor     ax, ax                  
     mov     ds, ax
